458b2f634ac33b70de2f993850aeb8aebfa5d737
Bo3lii <aoosa.alhaddad1@gmail.com>
2022-10-05 19:31:19 +0300

    al3bd agwa mn hu.....

---
+++ cw/cw/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  cw
+//
+//  Created by Aws Alhaddad on 5.10.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+var a = 3
+var c = 6
+v
---
+++ cw/cw/cwApp.swift
@@ -0,0 +1,17 @@
+//
+//  cwApp.swift
+//  cw
+//
+//  Created by Aws Alhaddad on 5.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct cwApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
